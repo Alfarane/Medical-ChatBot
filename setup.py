925bb143b6f234757c1b70352975350592a100bb
Alfarane <alpharane22@gmail.com>
2025-02-10 01:04:11 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Alpha Rane',
+    author_email= 'alpha.rane22@spit.ac.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
